e1452d566994499f3c1ba6f8a82f5e493765f392
Edwy MANDRET <emandret@student.42.fr>
2017-08-24 22:49:49 +0200

    ft_getpath freed

--- libft/src/libft/ft_getpath.c
+++ libft/src/libft/ft_getpath.c
@@ -6,7 +6,7 @@
 /*   By: emandret <emandret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/23 15:08:19 by emandret          #+#    #+#             */
-/*   Updated: 2017/07/23 15:09:33 by emandret         ###   ########.fr       */
+/*   Updated: 2017/08/24 22:47:20 by emandret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_getpath(char *path, char *file)
 {
-	if ('/' != path[ft_strlen(path) - 1])
-		path = ft_strjoin(path, "/");
+	if (file && '/' != path[ft_strlen(path) - 1])
+		return (ft_strjoin_free(ft_strjoin(path, "/"), file));
 	if (file)
 		return (ft_strjoin(path, file));
 	return (NULL);
